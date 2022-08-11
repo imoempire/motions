60c4fd25edc69d43ef20629c42bb83ed651d6e71
imoempire <mrimoempire@gmail.com>
2022-08-11 12:32:16 +0000

    modify

--- src/App.js
+++ src/App.js
@@ -1,23 +1,28 @@
-import './App.css';
+import "./App.css";
+import AboutUs from "./Components/About/About";
+import Header from "./Components/Header/Header";
+import Navbar from "./Components/Navbar/Navbar";
+import SpecialMenu from "./Components/Menu/SpecialMenu";
+import Chef from "./Components/Chef/Chef";
+import Intro from "./Components/Intro/Intro";
+import Laurels from "./Components/Laurels/Laurels";
+import Gallery from "./Components/Gallery/Gallery";
+import FindUs from "./Components/Findus/FindUs";
+import Footer from "./Components/Footer/Footer";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App = () => (
+  <div>
+    <Navbar />
+    <Header/>
+    <AboutUs/>
+    <SpecialMenu/>
+    <Chef />
+    <Intro />
+    <Laurels />
+    <Gallery />
+    <FindUs />
+    <Footer />
+  </div>
+);
 
 export default App;
